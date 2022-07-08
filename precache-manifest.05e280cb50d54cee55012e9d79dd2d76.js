89c6799e10175f4b31df61c662112536795d22d3
bertoldosi <matheus.bertoldosi@gmail.com>
2022-07-08 16:26:33 -0300

    Updates

---
+++ precache-manifest.05e280cb50d54cee55012e9d79dd2d76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc908a2d4b26e99588396d8b496011b",
+    "url": "/bertoldosi/github-explore/index.html"
+  },
+  {
+    "revision": "97e00fe625cd29253735",
+    "url": "/bertoldosi/github-explore/static/js/2.edc52022.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/bertoldosi/github-explore/static/js/2.edc52022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1809af524065e72256a4",
+    "url": "/bertoldosi/github-explore/static/js/main.1fbf8429.chunk.js"
+  },
+  {
+    "revision": "7b02db164a601449f141",
+    "url": "/bertoldosi/github-explore/static/js/runtime-main.c1040ec5.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/bertoldosi/github-explore/static/media/github.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/bertoldosi/github-explore/static/media/logo.04f6798f.svg"
+  }
+]);
